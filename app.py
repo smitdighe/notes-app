1cfadce6169472a9f191320420c8df013478d539
Smit Dighe <smitdighe@gmail.com>
2026-04-24 11:18:01 +0530

    Add Procfile and update app run configuration for deployment

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ def server_error(error):
     return render_template('500.html'), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
